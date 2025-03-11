f6e0f352cb51f6aa5a33bb245aea78252d67a33a
JakubSlomczewski <s25505@pjwstk.edu.pl>
2025-03-11 22:00:03 +0100

    Commit 1

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+
+int[] arr = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine(arr.Length);
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    } 
+    return sum / numbers.Length;
+}
+
+Console.WriteLine(GetAverage(arr));
