b123f2e663c7f82449f363c636edd0159463d6f4
JakubSlomczewski <s25505@pjwstk.edu.pl>
2025-03-11 22:19:22 +0100

    Zadanie4 - commit1

---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] arr = { 5, 1, 2, 10, 1, 2 };
+
+
+static int MaxValue(int[] numbers)
+{
+    int MaxNumber = numbers[0];
+    foreach(int number in numbers)
+        {
+            if( number > MaxNumber)
+             {
+                MaxNumber = number;
+             }
+        }
+    
+    return MaxNumber;
+}
+
+Console.WriteLine(MaxValue(arr));
